73805e84ed194f159bb75cd7f76fe6981b4c14d8
Kevin Funk <kevin@easypost.com>
2016-08-15 19:05:21 -0700

    add symlinked dir

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var path = require('path');
 
 gulp.task('images', function() {
-  return gulp.src(path.join(__dirname, 'images', '**', '*'))
+  return gulp.src(path.join(__dirname, 'images', '**', '*'), {followSymlinks: true})
     .pipe(gulp.dest(path.join(__dirname, 'output')));
 });
